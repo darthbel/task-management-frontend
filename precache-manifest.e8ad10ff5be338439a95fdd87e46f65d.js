ef7d1a1156fde67dc6f0da1c949b04ef276328fa
darthbel <darthbel@users.noreply.github.com>
2022-02-22 14:10:59 +0000

    deploy: 759db55a6d0b608596cba51c1bf48b7e52d64150

---
+++ precache-manifest.e8ad10ff5be338439a95fdd87e46f65d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fa0c57b118d53110403ac86f13d984",
+    "url": "https://darthbel.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "dc8d17828e5ab6cfe180",
+    "url": "https://darthbel.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "c478a3798d574ee2d5b6",
+    "url": "https://darthbel.github.io/task-management-frontend/static/js/2.b1306a03.chunk.js"
+  },
+  {
+    "revision": "dc8d17828e5ab6cfe180",
+    "url": "https://darthbel.github.io/task-management-frontend/static/js/main.6aeb260b.chunk.js"
+  },
+  {
+    "revision": "f5e9024be4848855770b",
+    "url": "https://darthbel.github.io/task-management-frontend/static/js/runtime~main.09b26d08.js"
+  }
+]);
